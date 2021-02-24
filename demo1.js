71c273f9d33717a94a52ec89e9342669ee5c2802
runnz <runnz121@gmail.com>
2021-02-24 23:57:41 +0900

    20210224 하위 버튼 제작 필요

--- demo1.js
+++ demo1.js
@@ -2,7 +2,7 @@
 //tab전환
 $(document).ready(function() {
     $('ul.tabs li').click(function(){
-        var tab_id = $(this).attr('data-tabb'); //클릭이벤트 발생한 data-tab값을 가져옴
+        var tab_id = $(this).attr('data-tab'); //클릭이벤트 발생한 data-tab값을 가져옴
         
         $('ul.tabs li').removeClass('current'); //선택 요소의  class를 제거
         $('.tab-content').removeClass('current');
@@ -12,17 +12,19 @@ $(document).ready(function() {
     })
 });
 
-$(document).ready(function() {
-    $('ul.tabs li').click(function(){
-        var tab_id = $(this).attr('data-tabb'); //클릭이벤트 발생한 data-tab값을 가져옴
-        
-        $('ul.tabs li').removeClass('current1'); //선택 요소의  class를 제거
-        $('.tab-content').removeClass('current1');
 
-        $(this).addClass('current1');
-        $('#'+tab_id).addClass('current1');    
+
+$(document).ready(function() {
+    $(".tab_under li").click(function() {
+      var idx = $(this).index();
+      $(".tab_under li").removeClass("on");
+      $(".tab_under li").eq(idx).addClass("on");
+      $(".tab_cont > div").hide();
+      $(".tab_cont > div").eq(idx).show();
     })
-});
+  });
+
+
 
 
 //dropdown button
